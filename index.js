d8d8b8964042eaf236cb4a40c4973deebb6b5dc9
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2024-04-27 13:33:03 +0530

    setup IRCTC Server

---
+++ index.js
@@ -0,0 +1,22 @@
+// src/app.js
+const express = require('express');
+const app = express();
+const apiRoutes = require('./src/routes/user')
+
+// const apiRoutes = require('./routes/api');
+
+// Middleware
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res ) => {
+    res.send('Welcome to Anakin IRCTC server!');
+});
+
+app.use('/api', apiRoutes );
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+// src/routes/api.js
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/user.controller');
+
+router.get('/users', userController.getAllUsers);
+
+module.exports = router;
