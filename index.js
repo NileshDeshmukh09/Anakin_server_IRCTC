3b31305eb99a642156345ed00e169ced800d72f5
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2024-04-28 10:01:32 +0530

    added flow chart

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-// src/app.js
-const express = require('express');
+
+const express = require("express");
 const app = express();
 const { specs, swaggerUi } = require("./swagger");
 const logger = require("morgan");
 const cors = require("cors");
-const apiRoutes = require('./src/routes')
-
-// const apiRoutes = require('./routes/api');
+const apiRoutes = require("./src/routes");
 
 // Middleware
 app.use(logger("dev"));
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 app.options("*", cors());
 
 // Routes
-app.get('/', (req, res ) => {
-    res.send('Welcome to Anakin IRCTC server!');
+app.get("/", (req, res) => {
+  res.send({ success: true, message: "Welcome to Anakin IRCTC server!" });
 });
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs)); 
-app.use('/api', apiRoutes );
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api", apiRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
